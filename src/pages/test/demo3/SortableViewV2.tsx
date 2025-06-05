1d319c1e92d46aa20fa1de00d7c77d4f4eef09a7
张日方 <wxzhangrifang@gfpartner.com.cn>
2025-06-05 13:36:11 +0800

    dnd-kit拖拽增加控制

--- src/pages/test/demo3/SortableViewV2.tsx
+++ src/pages/test/demo3/SortableViewV2.tsx
@@ -1,22 +1,37 @@
+import { DragDropProvider } from '@dnd-kit/react';
 import { useSortable } from '@dnd-kit/react/sortable';
+import { useState } from 'react';
+import { move } from '@dnd-kit/helpers';
 
 function Sortable({ id, index }) {
-  const { ref } = useSortable({ id, index });
+    const { ref } = useSortable({ id, index });
 
-  return (
-    <li ref={ref} className='item'>Item {id}</li>
-  );
+    return (
+        <li ref={ref} className='item'>{id}</li>
+    );
 }
 
 function SortableViewV2() {
-  const items = [1, 2, 3, 4];
+    const [items, setItems] = useState(['白日依山尽', '黄河入海流', '欲穷千里目', '更上一层楼']);
 
-  return (
-    <ul className='list'>
-      {items.map((id, index) =>
-        <Sortable key={id} id={id} index={index} />
-      )}
-    </ul>
-  );
+    return (
+        <DragDropProvider
+          onDragEnd={(event) => {
+              setItems((newItems) => {
+                newItems = move(newItems, event)
+                newItems.forEach((item, index) => {
+                    console.log(item, index) 
+                })
+                return newItems;
+            });
+          }}
+        >
+            <ul >
+                {items.map((id, index) => (
+                    <Sortable key={id} id={id} index={index} />
+                ))}
+            </ul>
+        </DragDropProvider>
+    );
 }
 export default SortableViewV2;
