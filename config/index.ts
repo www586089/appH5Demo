3440d721da63a2a888ea2cecbaaa9a59b6cd4b29
张日方 <wxzhangrifang@gfpartner.com.cn>
2025-08-14 15:37:56 +0800

    使用375设计稿

--- config/index.ts
+++ config/index.ts
@@ -8,7 +8,7 @@ export default defineConfig(async (merge, { command, mode }) => {
   const baseConfig: UserConfigExport = {
     projectName: 'appH5Demo',
     date: '2025-5-31',
-    designWidth: 750,
+    designWidth: 375,
     deviceRatio: {
       640: 2.34 / 2,
       750: 1,
@@ -81,7 +81,7 @@ export default defineConfig(async (merge, { command, mode }) => {
           config: {}
         },
         cssModules: {
-          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+          enable: true, // 默认为 false，如需使用 css modules 功能，则设为 true
           config: {
             namingPattern: 'module', // 转换模式，取值为 global/module
             generateScopedName: '[name]__[local]___[hash:base64:5]'
